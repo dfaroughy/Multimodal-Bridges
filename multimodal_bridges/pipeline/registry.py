1f880f7daa8e5d89f755e8ccc3360b139b6c9ba7
Darius Faroughy <dariusfar@gmail.com>
2025-02-25 10:26:13 -0500

    add OneCycleLR

--- multimodal_bridges/pipeline/registry.py
+++ multimodal_bridges/pipeline/registry.py
@@ -37,4 +37,5 @@ registered_schedulers = {
     "StepLR": torch.optim.lr_scheduler.StepLR,
     "ReduceLROnPlateau": torch.optim.lr_scheduler.ReduceLROnPlateau,
     "CosineAnnealingLR": torch.optim.lr_scheduler.CosineAnnealingLR,
+    "OneCycleLR": torch.optim.lr_scheduler.OneCycleLR,
 }
